eaabeec95b77e250905e5b22e62146b1e870a6b7
yazid <jidasilin@gmail.com>
2026-09-07 19:25:54 +0700

    laprak

---
+++ pekan1/praktikum1.py
@@ -0,0 +1,12 @@
+#
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("program ini menghasilkan")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\n"
+      "Muhammad Dzaky Yazid dan 2611531011")
