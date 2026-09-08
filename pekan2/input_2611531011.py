08b9c9a84e26ed61f9fc57d6d6fa31f9f5b02a5b
yazid <jidasilin@gmail.com>
2026-09-08 09:50:12 +0700

    tipe data

---
+++ pekan2/input_2611531011.py
@@ -0,0 +1,7 @@
+# program ini menggunakan fungsi input () untuk meminta pengguna
+# memasukkan nilai panjang dan lebar persegi panjang.
+# nilai yang dimasukkan akan dikonversi menjadi tipe data integer
+panjang_1011=int(input("masukkan nilai panjang:"))
+lebar_1011=int(input("masukkan nilai lebar: "))
+luas_1011=int(panjang_1011*lebar_1011)
+print("luas persegi panjang adalah: ", luas_1011)
