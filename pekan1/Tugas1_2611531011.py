99f492ed42321121a3f23cac15ac9ddc61367a05
yazid <jidasilin@gmail.com>
2026-09-08 22:12:42 +0700

    laprak1

---
+++ pekan1/Tugas1_2611531011.py
@@ -0,0 +1,6 @@
+print("tugas praktikum 1")
+print("nama: Muhammad Dzaky Yazid")
+print("nim: 2611531011")
+print("kelas: A")
+print("mata kuliah: Algoritma dan Pemrograman")
+print("dosen pengampu: Bapak Dedi Setiawan, S.Kom., M.Kom")
