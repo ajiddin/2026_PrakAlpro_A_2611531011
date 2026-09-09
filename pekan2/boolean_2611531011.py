369551e6518e28ce4bfcb062945cd96236569a84
yazid <jidasilin@gmail.com>
2026-09-09 23:00:50 +0700

    laprak

--- pekan2/boolean_2611531011.py
+++ pekan2/boolean_2611531011.py
@@ -1,6 +1,8 @@
-# deklarasi variabel dengan tipe data boolean
-is_lulus=True
-is_cumlaude=True
+# Buat file dengan nama Boolean_2611531021
+# Nama variabel ditambah 4 digit nim terakhir contoh: nilai_1234
+# Deklarasi variabel dengan tipe data Boolean
+is_lulus_1011=True
+is_cumlaude_1011=True
 
 # menggunakan boolean
 nilai_1011=85
@@ -12,5 +14,5 @@ status_kelulusan_1011=nilai_1011>=batas_lulus_1011
 print("=== check kelulusan ===")
 print("nilai :" , nilai_1011)
 print("apakah lulus? :", status_kelulusan_1011)
-if is_lulus and is_cumlaude:
+if is_lulus_1011 and is_cumlaude_1011:
     print("selamat anda lulus dengan predikat cum laude")
